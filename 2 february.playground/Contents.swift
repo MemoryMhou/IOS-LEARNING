7e64cf15a70dc8a7db6e333f35715c7e62a8b515
MemoryMhou <mhoumemory1@gmail.com>
2024-02-03 06:25:38 +0200

    Create functions with default parameters and variadic parameters.

--- 2 february.playground/Contents.swift
+++ 2 february.playground/Contents.swift
@@ -10,3 +10,25 @@ func addTwoNumbers(a: Int, b: Int) -> Int {
 
 let sum = addTwoNumbers(a: 5, b: 3)
 print("Sum: \(sum)")
+
+
+
+//Create functions with default parameters and variadic parameters.
+
+func greet(name: String = "Memory") {
+    print("Hello, \(name)!")
+}
+
+func sum(numbers: Int...) -> Int {
+    var result = 0
+    for number in numbers {
+        result += number
+    }
+    return result
+}
+
+greet()
+greet(name: "Peter")
+
+let total = sum(numbers: 1, 2, 3, 4, 5)
+print("Total: \(total)")
